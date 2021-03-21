c1e0544c6f6473d6b378c3217935f555f64ce024
David <davidbalyan9@gmail.com>
2021-03-21 21:17:05 +0200

    Added actual functionality with javascript

--- script.js
+++ script.js
@@ -1,136 +1,134 @@
-let zero = document.getElementById('0');
-let one = document.getElementById('1');
-let two = document.getElementById('2');
-let three = document.getElementById('3');
-let four = document.getElementById('4');
-let five = document.getElementById('5');
-let six = document.getElementById('6');
-let seven = document.getElementById('7');
-let eight = document.getElementById('8');
-let nine = document.getElementById('9');
-let plus = document.getElementById('plus');
-let minus = document.getElementById('minus');
-let mult = document.getElementById('mult');
-let div = document.getElementById('div');
-let clear = document.getElementById('AC');
-let equals = document.getElementById('equals');
-let equation = document.getElementById('equation-text');
+const numbers = document.querySelectorAll('[data-number]');
+const functions = document.querySelectorAll('[data-function]');
+const operators = document.querySelectorAll('[data-operator]');
+const equation = document.querySelector('[data-equation]');
+const result = document.querySelector('[data-result]');
+
+let previousOperator;
+let decimalExists;
+let operatorString = '';
+let isMaxed = false;
+
 let equationArray = [];
-let valueArray = [zero, one, two, three, four, five, six, seven, eight, nine, plus, minus, mult, div, equals];
-let num1, num2, sign;
+
+function clearWhenNewInput() {
+    if (result.innerText) {
+        result.innerText = '';
+        equation.innerText = '';
+        isMaxed = false;
+    }
+}
 
 function add(a, b) {
-    return a+b;
+    return a + b;
 }
 
 function subtract(a, b) {
-    return a-b;
-}
-
-function multiply(a, b) {
-    return a*b;
+    return a - b;
 }
 
 function divide(a, b) {
-    return a/b;
+    return a / b; 
 }
 
-function operate(operation, a, b) {
-    switch(operation) {
-        case 'add':
-            return add(a, b);
-        case 'subtract':
-            return subtract(a, b);
-        case 'multiply':
-            return multiply(a, b);
-        case 'divide':
-            return divide(a, b);
-    }
+function multiply(a, b) {
+    return a * b;
 }
 
-function displayValues() {
-    zero.onclick = () => {
-        equationArray.push(zero.innerText);
-        equation.innerText = equationArray.join('');
-    }
-
-    one.onclick = () => {
-        equationArray.push(one.innerText);
-        equation.innerText = equationArray.join('');
-        num1 = 1;
-    }
-    
-    two.onclick = () => {
-        equationArray.push(two.innerText);
-        equation.innerText = equationArray.join('');
-        num2 = 2;
-    }
-
-    three.onclick = () => {
-        equationArray.push(three.innerText);
-        equation.innerText = equationArray.join('');
-    }
-
-    four.onclick = () => {
-        equationArray.push(four.innerText);
-        equation.innerText = equationArray.join('');
-    }
-
-    five.onclick = () => {
-        equationArray.push(five.innerText);
-        equation.innerText = equationArray.join('');
-    }
-
-    six.onclick = () => {
-        equationArray.push(six.innerText);
-        equation.innerText = equationArray.join('');
-    }
-
-    seven.onclick = () => {
-        equationArray.push(seven.innerText);
-        equation.innerText = equationArray.join('');
-    }
-
-    eight.onclick = () => {
-        equationArray.push(eight.innerText);
-        equation.innerText = equationArray.join('');
-    }
-
-    nine.onclick = () => {
-        equationArray.push(nine.innerText);
-        equation.innerText = equationArray.join('');
-    }
-
-    plus.onclick = () => {
-        equationArray.push(plus.innerText);
-        equation.innerText = equationArray.join('');
-    }
-
-    minus.onclick = () => {
-        equationArray.push(minus.innerText);
-        equation.innerText = equationArray.join('');
-    }
-
-    mult.onclick = () => {
-        equationArray.push(mult.innerText);
-        equation.innerText = equationArray.join('');
-    }
-
-    div.onclick = () => {
-        equationArray.push(div.innerText);
-        equation.innerText = equationArray.join('');
-    }
-    
-    equals.onclick = () => {
-        equationArray.push(equals.innerText);
-        equationArray.push(num1 + num2);
-        equation.innerText = equationArray.join('');
-    }
-
-    clear.onclick = () => {
-        equationArray = [];
-        equation.innerText = equationArray.join('');
-    }
+function prohibitExtraOperators(op) {
+    equation.innerText += op;
+        previousOperator = true;
+        isMaxed = true;
 }
 
-displayValues();
+function numberClickHandler() {
+    numbers.forEach(number => {
+        let currentNumber = number.innerText;
+        number.addEventListener('click', () => {
+            if (currentNumber == '.') {
+                clearWhenNewInput();
+                if (!decimalExists) {
+                    equation.innerText += '.';
+                    decimalExists = true;
+                }
+                previousOperator = false;
+            } else {
+                for (let i = 0; i < 10; i++) {
+                    if (currentNumber == `${i}`) {
+                        equation.innerText += `${i}`;
+                        previousOperator = false;
+                    }
+                }
+            }
+        }); 
+    });
+}
+
+function functionClickHandler() {
+    functions.forEach(fun => { 
+        let currentFunction = fun.innerText;
+
+        fun.addEventListener('click', () => {
+            if (currentFunction == 'CLEAR') {
+                decimalExists = false;
+                previousOperator = false;
+                isMaxed = false;
+                equation.innerText = '';
+                result.innerText = '';
+            } else if (currentFunction == 'DELETE') {
+                result.innerText = '';
+                if (equation.innerText.slice(-1) == 'x' || equation.innerText.slice(-1) == '/' || equation.innerText.slice(-1) == '-' || equation.innerText.slice(-1) == '+') {
+                    previousOperator = false;
+                }
+                if (equation.innerText.slice(-1) == '.') {
+                    decimalExists = false;
+                }
+                equation.innerText = equation.innerText.slice(0, -1);
+            } else if (currentFunction == '=') {
+                if (operatorString == '+') {
+                    equationArray = equation.innerText.split('+');
+                    result.innerText = add(parseFloat(equationArray[0]), parseFloat(equationArray[1]));
+                } else if (operatorString == '-') {
+                    equationArray = equation.innerText.split('-');
+                    result.innerText = subtract(parseFloat(equationArray[0]), parseFloat(equationArray[1]));
+                } else if (operatorString == '/') {
+                    equationArray = equation.innerText.split('/');
+                    result.innerText = divide(parseFloat(equationArray[0]), parseFloat(equationArray[1]));
+                } else if (operatorString == 'x') {
+                    equationArray = equation.innerText.split('x');
+                    result.innerText = multiply(parseFloat(equationArray[0]), parseFloat(equationArray[1]));
+                }
+            }
+        });
+    });
+}
+
+function operatorClickHandler() {
+    operators.forEach(operator => {
+        let currentOperator = operator.innerText;
+
+        operator.addEventListener('click', () => {
+            if (currentOperator == '/' && !previousOperator && !isMaxed) {
+                operatorString = '/';
+                prohibitExtraOperators(currentOperator);
+            } else if (currentOperator == 'x' && !previousOperator && !isMaxed) {
+                operatorString = 'x';
+                prohibitExtraOperators(currentOperator);
+            } else if (currentOperator == '-' && !previousOperator && !isMaxed) {
+                operatorString = '-';
+                prohibitExtraOperators(currentOperator);
+            } else if (currentOperator == '+' && !previousOperator && !isMaxed) {
+                operatorString = '+';
+                prohibitExtraOperators(currentOperator);
+            }
+        });
+    });
+}
+
+function main() {
+    numberClickHandler();
+    operatorClickHandler();
+    functionClickHandler();
+}
+
+main();
